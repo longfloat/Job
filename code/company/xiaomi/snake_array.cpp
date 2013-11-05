a817b42406b825585a29324c36068d447223ff7f
longfloat <liutong0207@gmail.com>
2013-11-05 11:40:36 +0800

    xiaomi written test, snake array, space complexity with O(1)

--- code/company/xiaomi/snake_array.cpp
+++ code/company/xiaomi/snake_array.cpp
@@ -44,6 +44,39 @@ void print_snake(int n)
     array = NULL;
 }
 
+void print_snake2(int n)
+{
+    if (n < 0)
+        return;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            int m = i + j;
+            int tmp = m * (m + 1) / 2;
+            int sqr = n * n;
+            if (m < n)
+            {
+                tmp = m * (m + 1) / 2;
+                if (m & 2 != 0)
+                    cout << tmp + 1 + (m - j) << "\t";
+                else
+                    cout << tmp + 1 + (m - i) << "\t";
+            }
+            else
+            {
+                m = 2*n-2-m;
+                tmp = m * (m + 1) / 2;
+                if (m & 2 != 0)
+                    cout << sqr - (tmp + (m - (n-1-j))) << "\t";
+                else
+                    cout << sqr - (tmp + (m - (n-1-i))) << "\t";
+            }
+        }
+        cout << endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     freopen("snake_array.in", "r", stdin);
@@ -53,6 +86,7 @@ int main(int argc, char const *argv[])
         cout << "n = " << n << ":" << endl;
         print_snake(n);
         cout << endl;
+        print_snake2(n);
     }
     fclose(stdin);
     return 0;
